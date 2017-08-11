376728198e190f655ce1a63b27ede8cd1e80dc39
Orest Bryk <Orest Bryk>
2017-08-11 17:50:16 +0300

    New build

--- dist/app.js
+++ dist/app.js
@@ -692,7 +692,7 @@
 /***/ (function(module, exports) {
 
 	module.exports = {
-	    WIKI_PAGES_DISPLAY: 1000,
+	    WIKI_PAGES_DISPLAY: 10000,
 	    WIKI_PAGES_FETCH_URL: "https://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcprop=title%7Cids%7Csizes%7Cflags%7Cuser&rclimit=500&format=json"
 	};
 
